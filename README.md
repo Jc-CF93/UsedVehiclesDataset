a5ea564286a1fd9ab71fe15738e7c141276a4f72
Jc-CF93 <Jc-CF93@users.noreply.github.com>
2026-05-26 18:57:09 -0600

    Se crea VehiclesDataset y se le agregan funcionalidades, se altera el readme para explicar la aplicación, y se altera detalles del texto del EDA

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # UsedVehiclesDataset
 Aplicación web para visualizar la información de venta de automóviles usados
+
+La primera parte muestra el Data Frame para que el usuario pueda revisar toda la información.
+La siguiente sección se conforma por un casilla de verificación, que al ser seleccionada crea un histograma, el cual muestra la cantidad de vehiculos con relación al año del modelo.
+La ultima sección se conforma por un boton que al ser presionado crea un gráfico de dispersión que muestra la relación del precio con el año del modelo.
---
+++ vehiclesDataset.py
@@ -0,0 +1,46 @@
+# Importar paquetes
+import streamlit as st
+import pandas as pd
+import plotly.express as px
+
+# Leer el dataframe
+df_cars = pd.read_csv('vehicles_us.csv')
+
+
+st.header("Venta de automoviles usados")
+st.subheader("DataFrame")
+st.dataframe(df_cars)
+
+
+st.subheader("Histográma")
+hist_checkbox = st.checkbox('Construir histográma')
+
+if hist_checkbox:
+    st.write('Histográma del año de los vehículos')
+
+    # crear un histograma
+    fig = px.histogram(df_cars, x="model_year", labels={
+                       "model_year": "Año del modelo", "count": "Cantidad de vehículos"})
+
+    fig.update_layout(yaxis_title="Cantidad de vehículos")
+
+    # mostrar un gráfico Plotly interactivo
+    st.plotly_chart(fig, use_container_width=True)
+
+
+st.subheader('Gráfico de dispersión')
+df_year_price = df_cars.groupby('model_year')['price'].mean()
+df_year_price = df_year_price.reset_index()
+df_year_price['model_year'] = df_year_price['model_year'].astype(int)
+df_year_price['price'] = df_year_price['price'].astype(int)
+
+disp_button = st.button('Crear gráfico de dispersión')
+
+if disp_button:
+    st.write('Gráfico de dispersión del precio de los vehiculos')
+
+    fig_disp = px.scatter(
+        x=df_year_price['model_year'],
+        y=df_year_price['price'],
+        labels={"x": "Año del modelo", "y": "Precio"})
+    st.plotly_chart(fig_disp, use_container_width=True)
